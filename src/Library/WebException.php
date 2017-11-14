11fe678a40b3d331bc8ae85c9caf30948b5de462
zhaoye <zhaoye@youzan.com>
2017-11-14 13:09:16 +0800

    1.优化结构目录

---
+++ src/Library/WebException.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * author: zhaoye(zhaoye@youzan.com)
+ * Date: 2017/11/14
+ * Time: 上午11:53
+ */
+namespace Library;
+
+use Ywf\Core\Env;
+
+class WebException extends \Exception
+{
+    const INTER_ERROR = 220100100;
+    const PARAM_ERROR = 220100101;
+
+    public static $msgs = [
+        self::INTER_ERROR => '未知错误',
+        self::PARAM_ERROR => '参数错误',
+    ];
+
+
+    public function __construct($code, $param = "", $realCode = 0)
+    {
+        if ($realCode <= 0) {
+            $this->code = $code;
+        } else {
+            $this->code = $realCode;
+            $this->message = $param;
+            return;
+        }
+
+        $this->message = self::getErrorMsg($code);
+
+        $runMode = Env::get('run_mode');
+        if ('online' !== $runMode && $param != "") {
+            $this->message .= "_" . $param;
+        }
+    }
+
+    public static function getErrorMsg($errno)
+    {
+        if (isset(self::$msgs[$errno])) {
+            return self::$msgs[$errno];
+        } else {
+            return self::$msgs[self::INTER_ERROR];
+        }
+    }
+}
